093a9043c5ab75a9890c65b1609c4734d74a41de
Dávid Gurzó <dgurzo@gmail.com>
2021-03-29 19:17:39 +0200

    code smell fix

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -32,7 +32,6 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public boolean isDoorOpen() {
-		if(controller.getReferenceSpeed() > speedLimit) return true;
-		else return false;
+		return controller.getReferenceSpeed() > speedLimit;
 	}
 }
